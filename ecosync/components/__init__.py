c5318df22ab973da6abe54efe641b162a7fa11a2
SindhushreeNH <sindhuharish311@gmail.com>
2026-05-02 22:23:03 +0530

    Delete ecosync directory

--- ecosync/components/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dashboard import render_dashboard
-from .analytics import render_analytics
-from .scheduler import render_scheduler
-
-__all__ = ["render_dashboard", "render_analytics", "render_scheduler"]
--- ecosync/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .carbon_calculator import CarbonCalculator
-from .data_collector import DataCollector
-from .green_scheduler import GreenScheduler
-
-__all__ = ["CarbonCalculator", "DataCollector", "GreenScheduler"]
